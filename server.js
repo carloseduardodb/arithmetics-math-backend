05b99f213c8233583105d41b96bb0afe795d563d
carloseduardodb <carloscangere@hotmail.com>
2021-04-16 15:56:34 -0300

    add cross origin all address

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const path = require("path");
 
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 let messages = [];
 
-io.on("connection", (socket) => {
+io.on("connect", (socket) => {
   console.log(`Socket conectado ${socket.id}`);
 });
 
